62c33f16ae09f1e5faa342c1e6d9631f5f45c4e4
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2026-05-08 09:45:32 +0300

    Animated dialog text

--- game/scenes/HUD/dialog_ui.js
+++ game/scenes/HUD/dialog_ui.js
@@ -13,6 +13,8 @@ export default class DialogUI extends Scene {
         this.dialogText = null;
 
         this.storySystem = null;
+
+        this.dialogVisible = false;
     }
 
     init(data) {
@@ -51,9 +53,71 @@ export default class DialogUI extends Scene {
 
         this.dialogContainer.setSize(supposedWidth, supposedHeight);
         this.dialogBackground.setSize(supposedWidth, supposedHeight);
+
+        if (!this.dialogVisible) {
+            let tween = this.tweens.add({
+                targets: this.dialogContainer,
+                alpha: { from: 0, to: 1 },
+                y: { 
+                    from: this.sys.canvas.height,
+                    to: this.sys.canvas.height - 150
+                },
+                duration: 500,
+                ease: 'Power2',
+            })
+        }
+
+        this.animateText(this.dialogText);
+
+        this.dialogVisible = true;
     }
 
     hideDialog() {
         this.dialogContainer.setVisible(false);
+        this.dialogVisible = false;
     }
+
+    /**
+     * Create typewriter animation for text
+     * @param {Phaser.GameObjects.Text} target
+     * @param {number} [speedInMs=25]
+     * @returns {Promise<void>}
+     * author: Joel Thoms - https://joel.net/creating-a-typewriter-effect-in-phaserjs-v3
+     */
+    animateText(target, speedInMs = 25) {
+    // store original text
+    const message = target.text;
+    const invisibleMessage = message.replace(/[^ ]/g, " ");
+
+    // clear text on screen
+    target.text = "";
+
+    // mutable state for visible text
+    let visibleText = "";
+
+    // use a Promise to wait for the animation to complete
+    return new Promise((resolve) => {
+        const timer = target.scene.time.addEvent({
+        delay: speedInMs,
+        loop: true,
+        callback() {
+            // if all characters are visible, stop the timer
+            if (target.text === message) {
+            timer.destroy();
+            return resolve();
+            }
+
+            // add next character to visible text
+            visibleText += message[visibleText.length];
+
+            // right pad with invisibleText
+            const invisibleText = invisibleMessage.substring(visibleText.length);
+
+            // update text on screen
+            target.text = visibleText + invisibleText;
+        },
+        });
+    });
+    }
+
 }
